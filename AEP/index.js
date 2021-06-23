b29b5bb74bbcf6b1b4e99585c7e5e60352b33b78
Paulo H. Faria <67324352+Pa1loh@users.noreply.github.com>
2021-06-23 17:45:33 -0300

    Update

--- AEP/index.js
+++ AEP/index.js
@@ -1,10 +1,10 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
+    const uluru = { lat: -23.4202, lng: -51.9311 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
+      zoom: 13,
       center: uluru,
     });
     // The marker, positioned at Uluru
